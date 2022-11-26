993e3a4dd7f11077336aaafa18644add80e5852a
MilesVII <milesseventh@gmail.com>
2022-11-26 16:41:18 +0400

    Updated callmemaybe

--- api/callmemaybe.js
+++ api/callmemaybe.js
@@ -1,9 +1,8 @@
-
 const utils = require("./utils.js");
 
 export default function handler(request, response) {
-	let m = JSON.stringify(request.body);
-	let targetChat = request.query.to || process.env.TG_TARGET;
+	let m = JSON.stringify(request.body || "Bodyless request to callmemaybe");
+	let targetChat = request.query?.to || process.env.TG_T_ME;
 	if (!targetChat.startsWith("@")) targetChat = parseInt(targetChat, 10);
 	utils.genericTgRequest("sendMessage", {
 		chat_id: targetChat,
