086aa6df022bd7ca9646ea8850572d337d63b14d
MilesVII <milesseventh@gmail.com>
2023-02-28 01:21:18 +0400

    Completed feedback bot

--- api/prinzeugen/hook.js
+++ api/prinzeugen/hook.js
@@ -6,7 +6,7 @@ export default async function handler(request, response) {
 		response.status(200).send();
 		return;
 	}
-	
+
 	const me = process.env.TG_T_ME;
 	const token = process.env.PE_TG_TOKEN_SUPPORT;
 
@@ -14,20 +14,18 @@ export default async function handler(request, response) {
 		if (message.reply_to_message){
 			// me responding to ticket
 			const raw = message.reply_to_message.text?.split("\n");
-			const re = await tg("sendMessage", {
+			await tg("sendMessage", {
 				chat_id: parseInt(raw[0], 10),
 				text: message.text
 			}, token);
-			await tgReport(JSON.stringify(re));
 		}
 	} else {
-		const sender = `${message.from?.first_name || ""} ${message.from?.last_name || ""} ${message.from?.username || ""}`;
+		const sender = `${message.from?.first_name || ""} ${message.from?.last_name || ""} @${message.from?.username || ""}`;
 		const wooo = `${message.from.id}\n${sender} be like\n${message.text}`
-		const re = await tg("sendMessage", {
+		await tg("sendMessage", {
 			chat_id: me,
 			text: wooo
 		}, token)
-		await tgReport(JSON.stringify(re));
 	}
 	
 	response.status(200).send();
