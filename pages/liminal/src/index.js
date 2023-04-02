88faebd2c3f43d6f478ac03d784d32fdbff9dbf5
MilesVII <milesseventh@gmail.com>
2023-04-02 22:25:22 +0400

    Rehosted liminal

--- pages/liminal/src/index.js
+++ pages/liminal/src/index.js
@@ -18838,6 +18838,149 @@
     }
   }
 
+  // node_modules/three/examples/jsm/utils/BufferGeometryUtils.js
+  function mergeGeometries(geometries, useGroups = false) {
+    const isIndexed = geometries[0].index !== null;
+    const attributesUsed = new Set(Object.keys(geometries[0].attributes));
+    const morphAttributesUsed = new Set(Object.keys(geometries[0].morphAttributes));
+    const attributes = {};
+    const morphAttributes = {};
+    const morphTargetsRelative = geometries[0].morphTargetsRelative;
+    const mergedGeometry = new BufferGeometry();
+    let offset = 0;
+    for (let i = 0; i < geometries.length; ++i) {
+      const geometry = geometries[i];
+      let attributesCount = 0;
+      if (isIndexed !== (geometry.index !== null)) {
+        console.error("THREE.BufferGeometryUtils: .mergeGeometries() failed with geometry at index " + i + ". All geometries must have compatible attributes; make sure index attribute exists among all geometries, or in none of them.");
+        return null;
+      }
+      for (const name in geometry.attributes) {
+        if (!attributesUsed.has(name)) {
+          console.error("THREE.BufferGeometryUtils: .mergeGeometries() failed with geometry at index " + i + '. All geometries must have compatible attributes; make sure "' + name + '" attribute exists among all geometries, or in none of them.');
+          return null;
+        }
+        if (attributes[name] === void 0)
+          attributes[name] = [];
+        attributes[name].push(geometry.attributes[name]);
+        attributesCount++;
+      }
+      if (attributesCount !== attributesUsed.size) {
+        console.error("THREE.BufferGeometryUtils: .mergeGeometries() failed with geometry at index " + i + ". Make sure all geometries have the same number of attributes.");
+        return null;
+      }
+      if (morphTargetsRelative !== geometry.morphTargetsRelative) {
+        console.error("THREE.BufferGeometryUtils: .mergeGeometries() failed with geometry at index " + i + ". .morphTargetsRelative must be consistent throughout all geometries.");
+        return null;
+      }
+      for (const name in geometry.morphAttributes) {
+        if (!morphAttributesUsed.has(name)) {
+          console.error("THREE.BufferGeometryUtils: .mergeGeometries() failed with geometry at index " + i + ".  .morphAttributes must be consistent throughout all geometries.");
+          return null;
+        }
+        if (morphAttributes[name] === void 0)
+          morphAttributes[name] = [];
+        morphAttributes[name].push(geometry.morphAttributes[name]);
+      }
+      if (useGroups) {
+        let count;
+        if (isIndexed) {
+          count = geometry.index.count;
+        } else if (geometry.attributes.position !== void 0) {
+          count = geometry.attributes.position.count;
+        } else {
+          console.error("THREE.BufferGeometryUtils: .mergeGeometries() failed with geometry at index " + i + ". The geometry must have either an index or a position attribute");
+          return null;
+        }
+        mergedGeometry.addGroup(offset, count, i);
+        offset += count;
+      }
+    }
+    if (isIndexed) {
+      let indexOffset = 0;
+      const mergedIndex = [];
+      for (let i = 0; i < geometries.length; ++i) {
+        const index = geometries[i].index;
+        for (let j = 0; j < index.count; ++j) {
+          mergedIndex.push(index.getX(j) + indexOffset);
+        }
+        indexOffset += geometries[i].attributes.position.count;
+      }
+      mergedGeometry.setIndex(mergedIndex);
+    }
+    for (const name in attributes) {
+      const mergedAttribute = mergeAttributes(attributes[name]);
+      if (!mergedAttribute) {
+        console.error("THREE.BufferGeometryUtils: .mergeGeometries() failed while trying to merge the " + name + " attribute.");
+        return null;
+      }
+      mergedGeometry.setAttribute(name, mergedAttribute);
+    }
+    for (const name in morphAttributes) {
+      const numMorphTargets = morphAttributes[name][0].length;
+      if (numMorphTargets === 0)
+        break;
+      mergedGeometry.morphAttributes = mergedGeometry.morphAttributes || {};
+      mergedGeometry.morphAttributes[name] = [];
+      for (let i = 0; i < numMorphTargets; ++i) {
+        const morphAttributesToMerge = [];
+        for (let j = 0; j < morphAttributes[name].length; ++j) {
+          morphAttributesToMerge.push(morphAttributes[name][j][i]);
+        }
+        const mergedMorphAttribute = mergeAttributes(morphAttributesToMerge);
+        if (!mergedMorphAttribute) {
+          console.error("THREE.BufferGeometryUtils: .mergeGeometries() failed while trying to merge the " + name + " morphAttribute.");
+          return null;
+        }
+        mergedGeometry.morphAttributes[name].push(mergedMorphAttribute);
+      }
+    }
+    return mergedGeometry;
+  }
+  function mergeAttributes(attributes) {
+    let TypedArray;
+    let itemSize;
+    let normalized;
+    let arrayLength = 0;
+    for (let i = 0; i < attributes.length; ++i) {
+      const attribute = attributes[i];
+      if (attribute.isInterleavedBufferAttribute) {
+        console.error("THREE.BufferGeometryUtils: .mergeAttributes() failed. InterleavedBufferAttributes are not supported.");
+        return null;
+      }
+      if (TypedArray === void 0)
+        TypedArray = attribute.array.constructor;
+      if (TypedArray !== attribute.array.constructor) {
+        console.error("THREE.BufferGeometryUtils: .mergeAttributes() failed. BufferAttribute.array must be of consistent array types across matching attributes.");
+        return null;
+      }
+      if (itemSize === void 0)
+        itemSize = attribute.itemSize;
+      if (itemSize !== attribute.itemSize) {
+        console.error("THREE.BufferGeometryUtils: .mergeAttributes() failed. BufferAttribute.itemSize must be consistent across matching attributes.");
+        return null;
+      }
+      if (normalized === void 0)
+        normalized = attribute.normalized;
+      if (normalized !== attribute.normalized) {
+        console.error("THREE.BufferGeometryUtils: .mergeAttributes() failed. BufferAttribute.normalized must be consistent across matching attributes.");
+        return null;
+      }
+      arrayLength += attribute.array.length;
+    }
+    const array = new TypedArray(arrayLength);
+    let offset = 0;
+    for (let i = 0; i < attributes.length; ++i) {
+      array.set(attributes[i].array, offset);
+      offset += attributes[i].array.length;
+    }
+    return new BufferAttribute(array, itemSize, normalized);
+  }
+  function mergeBufferGeometries(geometries, useGroups = false) {
+    console.warn("THREE.BufferGeometryUtils: mergeBufferGeometries() has been renamed to mergeGeometries().");
+    return mergeGeometries(geometries, useGroups);
+  }
+
   // src/index.ts
   main();
   function chonks(src, length) {
@@ -18870,7 +19013,8 @@
     });
   }
   function slicesToVoxels(slices) {
-    const r = [];
+    const gDash = [];
+    const material = new MeshLambertMaterial({ vertexColors: true });
     slices.forEach((slice, x) => {
       chonks(slice.data, slice.dims.w).reverse().forEach((row, y) => {
         row.forEach((px, z) => {
@@ -18878,14 +19022,21 @@
             return;
           const color = new Color(px.r, px.g, px.b);
           const geometry = new BoxGeometry(1, 1, 1);
-          const material = new MeshLambertMaterial({ color });
-          const cube = new Mesh(geometry, material);
-          cube.position.set(x, y, z);
-          r.push(cube);
+          geometry.translate(x, y, z);
+          const positionAttribute = geometry.getAttribute("position");
+          const colors = [];
+          for (let _ = 0; _ < positionAttribute.count; _ += 3) {
+            colors.push(color.r, color.g, color.b);
+            colors.push(color.r, color.g, color.b);
+            colors.push(color.r, color.g, color.b);
+          }
+          geometry.setAttribute("color", new Float32BufferAttribute(colors, 3));
+          gDash.push(geometry);
         });
       });
     });
-    return r;
+    const g = mergeBufferGeometries(gDash);
+    return new Mesh(g, material);
   }
   function cctvPole(thickness, height, gab, plateFrame, plateAngle, color, display, camera, lights) {
     const epsilon = 0.1;
@@ -18897,9 +19048,7 @@
     const m1 = new Mesh(handle, mat);
     const m2 = new Mesh(plate, mat);
     m0.castShadow = true;
-    m0.receiveShadow = true;
     m1.castShadow = true;
-    m1.receiveShadow = true;
     m2.receiveShadow = true;
     const plateGroup = new Group();
     plateGroup.add(m2);
@@ -18921,6 +19070,9 @@
         l.target.position.set(t.x, t.y, t.z + 1);
         plateGroup.add(l);
         plateGroup.add(l.target);
+        l.castShadow = true;
+        l.shadow.mapSize.width = 1024;
+        l.shadow.mapSize.height = 1024;
       });
     }
     m0.translateY(height * 0.5);
@@ -18956,14 +19108,16 @@
       terrainTexture.wrapT = RepeatWrapping;
       terrainTexture.repeat.set(32, 32);
       const terrainMaterial = new MeshLambertMaterial({ map: terrainTexture });
-      const plane = new Mesh(new PlaneGeometry(1e3, 1e3), terrainMaterial);
-      plane.material.side = DoubleSide;
-      plane.rotation.x = Math.PI * 0.5;
-      plane.receiveShadow = true;
-      scene.add(plane);
+      const terrain = new Mesh(new PlaneGeometry(2048, 2048), terrainMaterial);
+      terrain.material.side = DoubleSide;
+      terrain.rotation.x = Math.PI * 0.5;
+      terrain.receiveShadow = true;
+      scene.add(terrain);
       const cubes = slicesToVoxels([side, semiddle, middle, middle, middle, semiddle, side]);
+      cubes.castShadow = true;
+      cubes.receiveShadow = true;
       const group = new Group();
-      cubes.forEach((c) => group.add(c));
+      group.add(cubes);
       const carBox = new Box3().setFromObject(group);
       const carCenter = new Vector3();
       carBox.getCenter(carCenter);
@@ -18972,7 +19126,6 @@
       group.translateY(0.5);
       const g2 = new Group();
       g2.add(group);
-      g2.castShadow = true;
       const headlights = [
         new SpotLight(new Color(0.9, 0.9, 1), 2.7, 100, Math.PI * 0.32),
         new SpotLight(new Color(0.9, 0.9, 1), 2.7, 100, Math.PI * 0.32)
@@ -18982,6 +19135,7 @@
       headlights.forEach((h) => {
         const t = h.position;
         h.target.position.set(t.x, t.y, t.z - 1);
+        h.castShadow = true;
       });
       group.add(...headlights);
       group.add(...headlights.map((h) => h.target));
@@ -18999,12 +19153,13 @@
       const cctv = new PerspectiveCamera(75, cctvFrame.x / cctvFrame.y, 0.1, 1e3);
       const cctvAngle = Math.PI * 0.2;
       const cctvLights = [
-        new SpotLight(new Color(0.9, 0.9, 1), 2.2, 400, Math.PI * 0.32),
-        new SpotLight(new Color(0.9, 0.9, 1), 2.2, 400, Math.PI * 0.32),
-        new SpotLight(new Color(0.9, 0.9, 1), 2.2, 400, Math.PI * 0.32)
+        new SpotLight(new Color(0.9, 0.9, 1), 1.3, 400, Math.PI * 0.32),
+        new SpotLight(new Color(0.9, 0.9, 1), 1.3, 400, Math.PI * 0.32),
+        new SpotLight(new Color(0.9, 0.9, 1), 1.3, 400, Math.PI * 0.32)
       ];
       const cctvPoleGroup = cctvPole(2, 42, 12, cctvDisplay, cctvAngle, new Color(0.42, 0.42, 0.42), cctvDisplayPlane, cctv, cctvLights);
-      cctvPoleGroup.position.set(-60, 0, -64);
+      cctvPoleGroup.position.set(0, 0, -64);
+      cctvPoleGroup.rotateY(Math.PI * 0.2);
       scene.add(cctvPoleGroup);
       const keyboardState = {};
       function keyEvent(isDown, keyCode) {
@@ -19041,11 +19196,11 @@
   }
   function mainloop(state) {
     requestAnimationFrame(() => mainloop(state));
-    const acceleration = 0.03;
-    const deceleration = 0.01;
+    const acceleration = 0.01;
+    const deceleration = 6e-3;
     const maxSpeed = 1;
-    const steeringPower = 0.01;
-    const desteeringPower = steeringPower * 0.32;
+    const steeringPower = 1e-3;
+    const desteeringPower = steeringPower * 0.7;
     const steeringMax = Math.PI * 0.02;
     if (state.keyboard["KeyW"] || state.keyboard["KeyS"]) {
       if (state.keyboard["KeyW"])
