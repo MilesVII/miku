c4c04c101525ba829556eac274e6905c6e8763e7
Miles Seventh <milesseventh@gmail.com>
2023-11-24 04:52:38 +0400

    Updated Rin

--- api/rin.js
+++ api/rin.js
@@ -1,18 +1,34 @@
 
 import * as RedisAccess from "./red.js"
-import { tgReport } from "./utils.js";
+import { tg, tgReport } from "./utils.js";
 
 export default async function handler(request, response) {
-	// let config = request.body?.config;
-	// let key = request.body?.key;
+	if (!request.body?.message) {
+		response.status(400).send();
+		return;
+	}
 
 	const dbCreds = {
 		host: process.env.RIN_REDIS_HOST,
 		pwd: process.env.RIN_REDIS_PWD,
 		port: process.env.RIN_REDIS_PORT
 	};
+	const rinToken = process.env.RIN_TG_TOKEN;
 	
 	//const v = await RedisAccess.get("tk", dbCreds);
+
+	// const tgr = await tg("setWebhook", {
+	// 	url: "https://mikumiku.vercel.app/api/rin",
+	// 	allowed_updates: "message"
+	// }, rinToken);
+
+	const msg = request.body.message.text;
+
+	const tgr = await tg("sendMessage", {
+		chat_id: request.body.message.chat.id,
+		text: msg,
+		reply_to_message_id: request.body.message.message_id
+	}, rinToken);
 
 	await tgReport(`intercept\n${typeof request.body}\n${JSON.stringify(request.body)}`, process.env.RIN_TG_TOKEN);
 
