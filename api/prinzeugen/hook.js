84ef935821406273964db5fdf01be0b3f59f6569
Miles Seventh <milesseventh@gmail.com>
2024-01-04 08:37:56 +0400

    Fixed imports

--- api/prinzeugen/hook.js
+++ api/prinzeugen/hook.js
@@ -1,4 +1,4 @@
-import { tg, tgReport } from "../utils";
+import { tg, tgReport } from "../utils.js";
 
 export default async function handler(request, response) {
 	const message = request.body?.message;
